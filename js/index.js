e36117486563aed07ee9faca62b0174bc1bb912f
John Pereira <john.epm1995@gmail.com>
2018-01-05 15:46:02 -0300

    Adicion Bootstrap

---
+++ js/index.js
@@ -0,0 +1,6 @@
+/*
+$(document).ready(function() {
+    var chat = $('#chat-widget');
+    chat.append('<div class="menu"><div class="back"><i class="fa fa-chevron-left"></i> <img src="https://i.imgur.com/DY6gND0.png" draggable="false"/></div><div class="name">Alex</div><div class="last">Activo</div></div>');
+});
+*/
