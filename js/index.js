3126a1dacf9c986d8f2e8c56571e062d2f82fc92
John Pereira <john.epm1995@gmail.com>
2018-01-06 14:35:03 -0300

    edicion casi lista del chat, estilos

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
-/*
 $(document).ready(function() {
     var chat = $('#chat-widget');
-    chat.append('<div class="menu"><div class="back"><i class="fa fa-chevron-left"></i> <img src="https://i.imgur.com/DY6gND0.png" draggable="false"/></div><div class="name">Alex</div><div class="last">Activo</div></div>');
+
+
+
+
+    //position: absolute; bottom:1%;right: .5%;padding:0; box-shadow: 0px 1px 6px 1px rgba(0,0,0,1); border-radius:5px
+    //chat.append('<div class="menu"><div class="back"><i class="fa fa-chevron-left"></i> <img src="https://i.imgur.com/DY6gND0.png" draggable="false"/></div><div class="name">Alex</div><div class="last">Activo</div></div>');
 });
-*/
