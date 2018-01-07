042a5de89d42d514653bfc1e34c64e10f3dc7c2d
John Pereira <john.epm1995@gmail.com>
2018-01-07 14:52:50 -0300

    diseño con sombras

--- js/index.js
+++ js/index.js
@@ -3,13 +3,14 @@ var sendMessage = $('#btn-send-message');
 var minimizeChat = $('#btn-minimize');
 var closeChat = $('#btn-close');
 var openchat = $('#open-chat');
-
+var inputMessage = $('#input-message');
 //chat panel builder
 
 //end chat panel builder
 
 $(document).ready(function() {
     chat.hide();
+    inputMessage.val('');
 
     openchat.click(function(){
         openchat.hide();
