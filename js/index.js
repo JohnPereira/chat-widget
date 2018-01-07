27e0ba3d535478e3711b20b99ea2e2c21b44fe6f
John Pereira <john.epm1995@gmail.com>
2018-01-07 02:53:17 -0300

    limpieza de codigo

--- js/index.js
+++ js/index.js
@@ -1,9 +1,33 @@
+var chat = $('#chat-widget');
+var sendMessage = $('#btn-send-message');
+var minimizeChat = $('#btn-minimize');
+var closeChat = $('#btn-close');
+var openchat = $('#open-chat');
+
+//chat panel builder
+
+//end chat panel builder
+
 $(document).ready(function() {
-    var chat = $('#chat-widget');
+    chat.hide();
 
+    openchat.click(function(){
+        openchat.hide();
+        chat.show();
+    });
 
+    minimizeChat.click(function(){
+        chat.hide();
+        openchat.show();
+    });
 
+    closeChat.click(function(){
+        chat.hide();
+        openchat.show();
+    });
 
-    //position: absolute; bottom:1%;right: .5%;padding:0; box-shadow: 0px 1px 6px 1px rgba(0,0,0,1); border-radius:5px
-    //chat.append('<div class="menu"><div class="back"><i class="fa fa-chevron-left"></i> <img src="https://i.imgur.com/DY6gND0.png" draggable="false"/></div><div class="name">Alex</div><div class="last">Activo</div></div>');
+    sendMessage.click(function(){
+        alert('asdkljaskld');
+    });    
 });
+
